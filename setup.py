ad293caa545a803ff8de1c954b78b661409b35ec
Ayaan Grover <ayaangrover@gmail.com>
2025-06-26 10:02:40 -0700

    Bump version to 0.1.7 and update distribution files
    Updated the package version to 0.1.7 in setup.py and PKG-INFO. Added new distribution files for the 0.1.7 release.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='rubble-lang',         
-    version='0.1.6',
+    version='0.1.7',
     description='An unreadable programming language.',
     author='Ayaan Grover',
     author_email='ayaangrover@gmail.com',
